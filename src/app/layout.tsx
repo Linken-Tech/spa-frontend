cf17a56a26fd170274729fa2425a615b23a79338
Daniel Lim Zheng Yu <daniellim@Daniels-MacBook-Pro.local>
2024-03-28 17:03:17 +0800

    re-structure nextjs file structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en h-full bg-gray-100">
-      <body className={[inter.className, "h-full"].join(" ")}>{children}</body>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
