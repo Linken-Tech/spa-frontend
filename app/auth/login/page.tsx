7c3f0afb41c4e9245d5eb955f6eabd0b3182ac0b
jasmine <cy.chong@up-devlabs.io>
2024-04-09 14:39:46 +0800

    [add] add login page and sample dashboard page

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,17 @@
+
+const Login = () => {
+    return (
+        <>
+            <div className="flex min-h-screen flex-row-reverse p-4">
+                <div className="bg-green-900 rounded-2xl flex-none w-2/5 h-1/2">
+                    <div className="flex item-center">
+                        <div className="w-32 h-96 bg-white rounded-lg">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login
